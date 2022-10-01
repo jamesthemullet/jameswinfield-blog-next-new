724209b342c938d6f301d2d3260f9658f49e4619
James Winfield <hello@iwillbeawebdeveloper.co.uk>
2022-10-01 17:13:06 +0100

    style nav and footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,14 +1,17 @@
 import Container from "./container";
+import Alert from "./alert";
 
 export default function Footer({ socials }) {
   return (
-    <footer className="bg-accent-1 border-t border-accent-2">
+    <footer className="bg-accent-1 border-t border-accent-2 bg-my-green">
       <Container>
         <div
-          className="flexbox-row"
+          className="flexbox-row text-white text-xl font-bold"
           dangerouslySetInnerHTML={{ __html: socials?.content }}
         />
       </Container>
+
+      <Alert preview={null} />
     </footer>
   );
 }
--- components/nav.tsx
+++ components/nav.tsx
@@ -2,12 +2,12 @@ import Link from "next/link";
 
 export default function Nav() {
   return (
-    <div className="flex flex-wrap justify-center items-center p-4 p-4 bg-accent-1">
+    <div className="flex flex-wrap justify-center items-center p-4 p-4 bg-my-green text-white">
       <Link href="/">
-        <a className="hover:underline pr-2 pl-2">Me</a>
+        <a className="hover:underline p-2 pl-4 pr-4 text-2xl font-bold">Me</a>
       </Link>
       <Link href="/blog">
-        <a className="hover:underline pr-2 pl-2">Blog</a>
+        <a className="hover:underline p-2 pl-4 pr-4 text-2xl font-bold">Blog</a>
       </Link>
     </div>
   );
