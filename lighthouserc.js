8209972b8b1b90f4bce97a31d443ebf3a7b6c7fa
James Winfield <hello@iwillbeawebdeveloper.co.uk>
2022-10-25 16:23:15 +0100

    add assertions

--- lighthouserc.js
+++ lighthouserc.js
@@ -2,8 +2,11 @@ module.exports = {
   ci: {
     collect: {
       url: ['http://localhost:3000/'],
-      numberOfRuns: 5,
+      numberOfRuns: 10,
       startServerCommand: 'yarn run dev',
+    },
+    assert: {
+      preset: 'lighthouse:recommended',
     },
     upload: {
       target: 'temporary-public-storage',
