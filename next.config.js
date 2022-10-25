fde164cf469cab31901c753d1a38137ad58fb156
James Winfield <hello@iwillbeawebdeveloper.co.uk>
2022-10-25 15:27:58 +0100

    Set config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 if (!process.env.WORDPRESS_API_URL) {
+  console.log(10, process.env);
   throw new Error(`
     Please provide a valid WordPress instance URL.
     Add to your environment variables WORDPRESS_API_URL.
