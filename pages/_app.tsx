09644914268c3887acb46f32b19cadd05355841c
James Winfield <hello@iwillbeawebdeveloper.co.uk>
2023-02-07 18:13:56 +0000

    add analytics tag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,28 @@
 import { AppProps } from 'next/app';
 import '../styles/index.css';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-THF64Z6X3S`}
+      />
+
+      <Script strategy="lazyOnload">
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+      
+        gtag('config', 'G-THF64Z6X3S');
+    `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
