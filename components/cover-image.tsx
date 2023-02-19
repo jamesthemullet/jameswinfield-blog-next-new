aa0c80baf9222c4e202107980c32de8319985e1d
James Winfield <hello@jameswinfield.co.uk>
2023-02-19 08:03:55 +0000

    test

--- components/cover-image.tsx
+++ components/cover-image.tsx
@@ -21,12 +21,11 @@ export default function CoverImage({ title, coverImage, slug }: Props) {
     <Image
       width={coverImage?.node.mediaDetails.width}
       height={coverImage?.node.mediaDetails.height}
-      alt={`Cover Image for ${title}`}
+      alt={`Cover Imagey for ${title}`}
       src={coverImage?.node.sourceUrl}
       className={cn('shadow-small', {
         'hover:shadow-medium transition-shadow duration-200': slug,
       })}
-      priority
     />
   );
 
