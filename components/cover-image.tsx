66636e89a134d7ddd1c53d3341a28854429a2e43
James Winfield <hello@jameswinfield.co.uk>
2023-02-19 08:12:59 +0000

    treosh v9

--- components/cover-image.tsx
+++ components/cover-image.tsx
@@ -21,11 +21,12 @@ export default function CoverImage({ title, coverImage, slug }: Props) {
     <Image
       width={coverImage?.node.mediaDetails.width}
       height={coverImage?.node.mediaDetails.height}
-      alt={`Cover Imagey for ${title}`}
+      alt={`Cover Image for ${title}`}
       src={coverImage?.node.sourceUrl}
       className={cn('shadow-small', {
         'hover:shadow-medium transition-shadow duration-200': slug,
       })}
+      priority
     />
   );
 
