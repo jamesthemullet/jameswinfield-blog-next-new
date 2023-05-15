262a2664b8cde2dfe6cd726176a81d472265b2c8
James Winfield <hello@iwillbeawebdeveloper.co.uk>
2023-05-15 19:49:19 +0100

    fix comments

--- components/comments.tsx
+++ components/comments.tsx
@@ -1,36 +1,31 @@
-import CommentForm from './commentForm';
 import Date from './date';
 
 type CommentsProps = {
   comments: {
-    edges: {
-      node: {
-        author: {
-          node: {
-            name: string;
-            avatar: {
-              url: string;
-            };
+    node: {
+      author: {
+        node: {
+          name: string;
+          avatar: {
+            url: string;
           };
         };
-        id: number;
-        content: string;
-        date: string;
       };
-    }[];
-  };
-  id: number;
+      id: number;
+      content: string;
+      date: string;
+    };
+  }[];
 };
 
-export default function Comments({ comments, id }: CommentsProps) {
-  const commentsToDisplay = comments?.edges;
+export default function Comments({ comments }: CommentsProps) {
   return (
     <section>
       <h2 className="mb-8 text-5xl md:text-5xl font-bold tracking-tighter leading-tight">
         Comments
       </h2>
       <div className="max-w-2xl mx-auto">
-        {commentsToDisplay.map(({ node }) => (
+        {comments.map(({ node }) => (
           <div key={node.id} className="mb-4 max-w-2xl mx-auto">
             <div className="mb-4" dangerouslySetInnerHTML={{ __html: node.content }} />
             <p>{node.author.node.name}</p>
@@ -38,7 +33,6 @@ export default function Comments({ comments, id }: CommentsProps) {
           </div>
         ))}
       </div>
-      <CommentForm postId={id} />
     </section>
   );
 }
