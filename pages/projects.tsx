bb59e2cb9ed602160eb19466e75d55e54e80b01e
James Winfield <hello@iwillbeawebdeveloper.co.uk>
2023-06-14 09:04:27 +0100

    projects page

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -8,8 +8,13 @@ import PostBody from '../components/post-body';
 import { getPage, getSocials, getGithubProjects } from '../lib/api';
 import type { PageProps } from '../lib/types';
 
-export default function Projects({ socials, page }: PageProps) {
+export default function Projects({ page, socials, projects }: PageProps) {
   const { content, seo } = page;
+  console.log(1, projects);
+  const projectsToDisplay = projects
+    .filter((project) => !project.isPrivate)
+    .sort((a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime());
+  console.log(2, projectsToDisplay);
   return (
     <Layout socials={socials} seo={seo}>
       <Head>
@@ -20,6 +25,28 @@ export default function Projects({ socials, page }: PageProps) {
         <>
           <Intro />
           <PostBody content={content} />
+          <div className="grid grid-cols-1 gap-4 mb-12 max-w-2xl mx-auto">
+            {projectsToDisplay.map((project) => (
+              <div key={project.id} className="p-4 border border-2 border-my-blue rounded">
+                <h2 className="text-2xl font-bold mb-2">{project.name}</h2>
+                <p className="text-black mb-2">{project.description}</p>
+                <a
+                  href={project.url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-gray-500 block">
+                  {project.url}
+                </a>
+                <a
+                  href={project.homepageUrl}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-gray-500 block">
+                  {project.homepageUrl}
+                </a>
+              </div>
+            ))}
+          </div>
         </>
       </Container>
     </Layout>
@@ -32,7 +59,7 @@ export const getStaticProps: GetStaticProps = async () => {
   const projects = await getGithubProjects();
   console.log(10, projects);
   return {
-    props: { page, socials },
+    props: { page, socials, projects },
     revalidate: 10,
   };
 };
