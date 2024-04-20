4132973abf4f17d15be9876aa02d010f41179557
James Winfield <hello@iwillbeawebdeveloper.co.uk>
2024-04-20 13:22:58 +0100

    fix paragraphs

--- components/post-body.tsx
+++ components/post-body.tsx
@@ -14,7 +14,7 @@ export default function PostBody({ content }: PostBodyProps) {
     return p;
   });
 
-  const newContentString = newContent.join('');
+  const newContentString = newContent.join('<br />');
   return (
     <div className="max-w-4xl mx-auto">
       <div className={styles.content} dangerouslySetInnerHTML={{ __html: newContentString }} />
